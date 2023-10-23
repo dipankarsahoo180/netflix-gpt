c2a26e8ef99aa3e3e667df48b60a4acd6cc1ca63
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2023-10-23 15:23:21 +0530

    without open api key

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-   apiKey: process.env.REACT_APP_TMDB_KEY, // defaults to process.env["OPENAI_API_KEY"] //REACT_APP_ prefix is mandatory for react to understand API
-   dangerouslyAllowBrowser: true,
-});
+const openai = null;
+// new OpenAI({
+//    apiKey: process.env.OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"] //REACT_APP_ prefix is mandatory for react to understand API
+//    dangerouslyAllowBrowser: true,
+// });
 
 export default openai;
